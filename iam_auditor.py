370ac0fc0bd0761eeeb7a6ca7d3d2a18d68bd311
Jamal Henderson <jamtmac1@gmail.com>
2025-04-30 23:48:46 -0400

    Added escalation actions

--- iam_auditor.py
+++ iam_auditor.py
@@ -1,6 +1,17 @@
 import boto3
+import json
 
 iam = boto3.client('iam')
+
+# Added list of roles
+escalation_actions = [
+	"iam:CreateUser",
+	"iam:PutUserPolicy",
+	"iam:AddUserToGroup",
+	"iam:PassRole"
+	"iam:create access key"
+
+]
 def list_roles():
     response = iam.list_roles()
     for role in response['Roles']:
