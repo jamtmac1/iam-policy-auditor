314eaabab0c5c0518fd3024b65cdb1534de5e105
Jamal Henderson <jamtmac1@gmail.com>
2025-05-22 22:24:41 -0400

    clean up roles actions

--- iam_auditor.py
+++ iam_auditor.py
@@ -9,13 +9,8 @@ escalation_actions = [
 	"iam:CreateUser",
 	"iam:PutUserPolicy",
 	"iam:AddUserToGroup",
-<<<<<<< HEAD
-	"iam:PassRole"
-	"iam:Createaccesskey"
-=======
 	"iam:PassRole",
-	"iam:CreateAccessKey"
->>>>>>> b7d1ac23766271c30a71d5696083186866796015
+	"iam:Createaccesskey"
 
 # Function to list and export results
 ]
@@ -23,7 +18,7 @@ escalation_actions = [
 def list_roles():
 	with open("'iam_audit_results.csv", mode = "w", newline="") as csvfile:
 		writer = csvwriter(csvfile)
-		write.writerow(["RolenName","Wildcard","EscalationRisks". 
+		write.writerow(["RolenName","Wildcard","EscalationRisks"])
     response = iam.list_roles()
 
 with open('iam_audit_results.csv', mode='w', newline= '') as csvfile:
@@ -31,7 +26,10 @@ with open('iam_audit_results.csv', mode='w', newline= '') as csvfile:
 	writer.writerow(["roleName", "Wildcards", "EscalationRisks"])
 
 	for role in response['Roles']:
-        print(f"Role: {role['RoleName']}")
+        equal_wildcards = []
+	equal_escalations = []
+        
+#print(f"Role: {role['RoleName']}")
 
 if __name__ == "__main__":
     list_roles()
